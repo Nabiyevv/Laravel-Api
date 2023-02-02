0dc763132571b670e5501bc1e63c146a8b23174c
Mahamad Nabiyev <maqa.nabiev@gmail.com>
2023-02-02 15:07:31 +0400

    Products and Reviews migrations created

--- database/migrations/2023_02_02_074735_create_products_table.php
+++ database/migrations/2023_02_02_074735_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             //$table->id();
             $table->increments('id');
-            $table->string('name');
+            //$table->string('name');
             $table->text('detail');
             $table->integer('price');
             $table->integer('stock');
